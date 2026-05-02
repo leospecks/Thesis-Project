0d5b7e90f9ea06f5c072e0cf982716f091d52efd
Leo Specks <specksleo@gmail.com>
2026-05-02 10:46:47 +0200

    changes

--- 6_frontend/streamlit_app.py
+++ 6_frontend/streamlit_app.py
@@ -6,8 +6,9 @@ import streamlit as st
 from pathlib import Path
 from datetime import datetime
 
-
 st.set_page_config(page_title="Thesis Dashboard", layout="wide")
+
+ROOT = Path(__file__).resolve().parent.parent
 
 st.title("Thesis Dashboard")
 st.text("Can Machine Learning Variable Selection and Individual Commodity Fundamentals Improve Futures Return Forecasting?")
@@ -15,36 +16,36 @@ st.link_button("🔗 Github repository", "https://github.com/leospecks/Thesis-Pr
 
 st.header("Current Draft")
 
-pdf_path = Path("1_latex/build/main.pdf")
+pdf_path = ROOT / "1_latex" / "build" / "main.pdf"
 
 if pdf_path.exists():
-    today = datetime.now().strftime("%Y-%m-%d")  # e.g. 2026-04-18
+    today = datetime.now().strftime("%Y-%m-%d")
     filename = f"thesis_leospecks_{today}.pdf"
     with open(pdf_path, "rb") as f:
         st.download_button(
             label="⬇️ Download PDF",
             data=f,
-            file_name= filename,
+            file_name=filename,
             mime="application/pdf"
         )
     st.pdf(str(pdf_path), height=500)
 else:
-    st.error(f"PDF not found: {pdf_path.resolve()}")
-
+    st.error(f"PDF not found: {pdf_path}")
 
 st.header("Presentation")
-pdf_path2 = Path("7_docs/defence_presentation.pdf")
+
+pdf_path2 = ROOT / "7_docs" / "defence_presentation.pdf"
 
 if pdf_path2.exists():
-    today = datetime.now().strftime("%Y-%m-%d")  # e.g. 2026-04-18
+    today = datetime.now().strftime("%Y-%m-%d")
     filename = f"presentation_leospecks_{today}.pdf"
     with open(pdf_path2, "rb") as f:
         st.download_button(
             label="⬇️ Download PDF",
             data=f,
-            file_name= filename,
+            file_name=filename,
             mime="application/pdf"
         )
     st.pdf(str(pdf_path2), height=500)
 else:
-    st.error(f"PDF not found: {pdf_path2.resolve()}")
+    st.error(f"PDF not found: {pdf_path2}")
